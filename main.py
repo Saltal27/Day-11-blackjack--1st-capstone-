81541bb53d8a0f9ca67ab7965214873f2a03920c
Sal Tal <110280707+Saltal27@users.noreply.github.com>
2022-10-22 11:39:33 +0000

    Finished the code (probably)

--- main.py
+++ main.py
@@ -11,72 +11,108 @@ d2 = random.choice(cards)
 
 
 def blackjack():
-    player_cards = {"p1": p1, "p2": p2}
-    player_cards_values = [p1, p2]
-    player_cards_summation = p1 + p2
-    player_cards_number = 2
+  player_cards = {"p1": p1, "p2": p2}
+  player_cards_values = [p1, p2]
+  player_cards_summation = p1 + p2
+  player_cards_number = 2
 
-    dealer_cards = {"d1": d1, "d2": d2}
-    dealer_cards_values = [d1, d2]
-    dealer_cards_summation = d1 + d2
-    dealer_cards_number = 2
+  dealer_cards = {"d1": d1, "d2": d2}
+  dealer_cards_values = [d1, d2]
+  dealer_cards_summation = d1 + d2
+  dealer_cards_number = 2
 
-    print(logo)
-    print(
-        f"Your cards: {player_cards_values}, current score: {player_cards_summation}"
-    )
-    print(f"Computer's first card: {d1}")
+  print(logo)
+  print(f" Your cards: {player_cards_values}, current score: {player_cards_summation}")
+  print(f" Computer's first card: {d1}")
 
-    stand = False
-    while not stand:
-        hit = input("Type 'y' to get another card, type 'n'to pass:\n").lower()
-        if hit == 'y':
-            player_cards_number += 1
-            player_cards[f"p{player_cards_number}"] = random.choice(cards)
+  stand = False
+  while stand == False and player_cards_summation <= 21:
+    hit = input("Type 'y' to get another card, type 'n'to pass:\n").lower()
+    if hit == 'y':
+      player_cards_number += 1
+      player_cards[f"p{player_cards_number}"] = random.choice(cards)
 
-            player_cards_values = []
-            for card in player_cards:
-                player_cards_values.append(player_cards[card])
+      player_cards_values = []
+      for card in player_cards:
+        player_cards_values.append(player_cards[card])
 
-            player_cards_summation = 0
-            for card in player_cards_values:
-                player_cards_summation += card
+      player_cards_summation = 0
+      for card in player_cards_values:
+        player_cards_summation += card
 
-            print(
-                f"Your cards: {player_cards_values}, current score: {player_cards_summation}"
-            )
-            print(f"Computer's first card: {d1}")
+      if player_cards_summation > 21 and 11 in player_cards_values:
+        ace_position = player_cards_values.index(11)
+        player_cards_values[ace_position] = 1
+        player_cards_summation -= 10
+        first_ace = True
+        while first_ace == True:
+          for card in player_cards:
+            if player_cards[card] == 11:
+              player_cards[card] = 1
+              first_ace = False
 
-        elif hit == 'n':
-            stand = True
-            while dealer_cards_summation < 17:
-                dealer_cards_number += 1
-                dealer_cards[f"d{dealer_cards_number}"] = random.choice(cards)
+      print(f" Your cards: {player_cards_values}, current score: {player_cards_summation}")
+      print(f" Computer's first card: {d1}")
 
-                dealer_cards_values = []
-                for card in dealer_cards:
-                    dealer_cards_values.append(dealer_cards[card])
+    elif hit == 'n':
+      stand = True
+    else:
+      print(" Invalid info!")
 
-                dealer_cards_summation = 0
-                for card in dealer_cards_values:
-                    dealer_cards_summation += card
+  while dealer_cards_summation < 17:
+    dealer_cards_number += 1
+    dealer_cards[f"d{dealer_cards_number}"] = random.choice(cards)
 
-            print(
-                f"Your final hand: {player_cards_values}, final score: {player_cards_summation}"
-            )
-            print(
-                f"Computer's final hand: {dealer_cards_values}, final score: {dealer_cards_summation}"
-            )
-        else:
-            print("Invalid info!")
+    dealer_cards_values = []
+    for card in dealer_cards:
+      dealer_cards_values.append(dealer_cards[card])
 
+    dealer_cards_summation = 0
+    for card in dealer_cards_values:
+      dealer_cards_summation += card
 
-play = input(
-    "Do you want to play a game of Blackjack?\nType 'y' or 'n':\n").lower()
-if play == 'y':
+    if dealer_cards_summation > 21 and 11 in dealer_cards_values:
+      ace_position = dealer_cards_values.index(11)
+      dealer_cards_values[ace_position] = 1
+      dealer_cards_summation -= 10
+      first_ace = True
+      while first_ace == True:
+        for card in dealer_cards:
+          if dealer_cards[card] == 11:
+            dealer_cards[card] = 1
+            first_ace = False
+
+  print(f" Your final hand: {player_cards_values}, final score: {player_cards_summation}")
+  print(f" Computer's final hand: {dealer_cards_values}, final score: {dealer_cards_summation}")
+
+  if player_cards_summation > 21:
+    print("You went over.\nYou lose :(")
+  else:
+    if dealer_cards_summation > 21:
+      print("Opponent went over.\nYou win :)")
+    else:
+      if player_cards_summation > dealer_cards_summation:
+        print("You win ツ")
+      elif player_cards_summation == dealer_cards_summation:
+        print("Tie *_*")
+      else:
+        print("You lose (╥﹏╥)")
+
+  again = input("Do you want to play another game of blackjack?\nType 'y' or 'n':\n").lower()
+  if again == 'y':
     clear()
     blackjack()
-elif play == 'n':
-    print("Understandable. Have a great day (￣^￣)ゞ")
-else:
+  elif again == 'n':
+    print("Understandable. Have a great day ╍●‿●╍")
+  else:
     print("Invalid info!")
+
+
+play = input("Do you want to play a game of Blackjack?\nType 'y' or 'n':\n").lower()
+if play == 'y':
+  clear()
+  blackjack()
+elif play == 'n':
+  print("Understandable. Have a great day ╍●‿●╍")
+else:
+  print("Invalid info!")
